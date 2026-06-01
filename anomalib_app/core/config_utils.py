604766c8a85a4982dcc9b6e14c0c25d08663304e
master <master@email.com>
2026-06-01 14:52:45 +0900

    Handle different config file locations between debug and runtime environments

--- anomalib_app/core/config_utils.py
+++ anomalib_app/core/config_utils.py
@@ -1,13 +1,22 @@
 import json
 import logging
+import os
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
 
 def get_config():
-    config_path = (
-        Path(__file__).resolve().parent.parent.parent / "config.json"
-    )
+
+    if os.getenv("DEBUG_MODE") == "True":
+        # When running in debug mode, the config file is located one level up from the current directory
+        config_path = (
+            Path(__file__).resolve().parent.parent.parent / "config.json"
+        )
+    else:
+        # When running in normal mode, the config file is located two levels up from the current directory
+        config_path = (
+            Path(__file__).resolve().parent.parent.parent.parent / "config.json"
+        )
     with open(config_path, "r", encoding="utf-8") as f:
         return json.load(f)
